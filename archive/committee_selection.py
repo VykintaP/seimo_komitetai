7aea9ea2192026b40dc9ed0d9366c3c1c46346ff
Vykinta <vykinta.parciauskaite@gmail.com>
2025-06-08 17:32:42 +0300

    Pridėtas .gitignore failas atgal

---
+++ archive/committee_selection.py
@@ -0,0 +1,10 @@
+from dash import Input, Output, callback
+
+@callback(
+    Output("selected-committee", "data"),
+    Input("committee-bar", "clickData")
+)
+def store_committee_selection(clickData):
+    if clickData and "points" in clickData:
+        return clickData["points"][0]["y"]
+    return None
