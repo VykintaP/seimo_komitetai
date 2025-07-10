247d1d4a77051a986df050875a52bf56abdac992
Vykinta <vykinta.parciauskaite@gmail.com>
2025-07-10 19:34:22 +0300

    comments

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,11 +1,11 @@
 -- Lentelė saugo klasifikuotų komitetų posėdžių klausimų duomenis
 CREATE TABLE IF NOT EXISTS classified_questions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    komitetas TEXT,  # Komiteto pavadinimas 
-    data TEXT,  # Posėdžio data
-    klausimas TEXT,  # Svarstomo klausimo tekstas
-    tema TEXT,  # Priskirta tematika
-    projektas TEXT,  # Svarstomo projekto numeris
-    atsakingi TEXT,  # Atsakingi asmenys
-    dalyviai TEXT  # Kviesti dalyvauti asmenys
+    komitetas TEXT,
+    data TEXT,
+    klausimas TEXT,
+    tema TEXT,
+    projektas TEXT,
+    atsakingi TEXT,
+    dalyviai TEXT
 );
